07db9c8ce1c3a335c031b34665e0d5f23e4948ce
drissakande <114373066+Kandedriva@users.noreply.github.com>
2025-06-18 05:12:16 -0400

    More change.

--- backend/index.js
+++ backend/index.js
@@ -362,7 +362,7 @@ app.get("/productList", async (req, res) => {
 //     }
 // });
 
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
